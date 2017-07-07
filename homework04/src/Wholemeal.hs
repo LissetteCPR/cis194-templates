30ae84ee1f0da6992245c46b49768448937aac85
Lissette Ponguillo <liss_cpr@hotmail.com>
2017-07-07 11:19:56 -0500

    Adding solutions of Homework 4

--- homework04/src/Wholemeal.hs
+++ homework04/src/Wholemeal.hs
@@ -14,8 +14,8 @@ module Wholemeal where
 fun1 :: [Integer] -> Integer
 fun1 []     = 1
 fun1 (x:xs)
-    | even x    = (x - 2) * fun1 xs
-    | otherwise = fun1 xs
+  | even x    = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
 
 fun2 :: Integer -> Integer
 fun2 1 = 0
@@ -27,7 +27,7 @@ fun2 n
 --
 -- >>> fun1 [1,3,5,7] == fun1' [1,3,5,7]
 -- True
--- >>> fun1 [1,2,3] /= fun1' [1,2,3]
+-- >>> fun1 [1,2,3] /= fun1' [1,2,3]                                                         
 -- False
 -- >>> fun2 10 == fun2' 10
 -- True
@@ -35,23 +35,32 @@ fun2 n
 -- False
 
 fun1' :: [Integer] -> Integer
-fun1' = undefined
+fun1' xs = product (map (subtract 2) (filter even xs))
 
 fun2' :: Integer -> Integer
-fun2' = undefined
+fun2'  = sum . filter even . takeWhile (>1) . iterate (\n->if even n then n `div` 2 else 3*n + 1)
+
 
 ----------------------------------------------------------------------
 -- Exercise 2
 ----------------------------------------------------------------------
 
-data Tree a =
+data Tree a = 
     Leaf
-  | Node Integer (Tree a) a (Tree a)
+  | Node Int (Tree a) a (Tree a)
     deriving (Show, Eq)
 
-foldTree :: [a] -> Tree a
-foldTree = undefined
+mitades :: [a] -> ([a],[a])
+mitades xs = splitAt (length xs `div` 2) xs 
 
+foldTree :: [a] -> Tree a
+foldTree [] = Leaf
+foldTree [x] = Node 0 Leaf x Leaf
+foldTree xs = Node acc (foldTree ys) z (foldTree zs)
+  where (ys,z:zs) = mitades xs
+        acc = (length xs `div` 2) -1
+   
+                
 ----------------------------------------------------------------------
 -- Exercise 3
 ----------------------------------------------------------------------
@@ -63,16 +72,23 @@ foldTree = undefined
 -- >>> xor [False, True, False, False, True]
 -- False
 
+xorFilter :: [Bool] -> [Bool]
+xorFilter = foldr (\x y -> if x then x:y else y) []
+  
 xor :: [Bool] -> Bool
-xor = undefined
+xor xs  = odd $ length (xorFilter xs)
 
+{-xorFilter2 :: [Bool] -> [Bool]
+xorFilter2 = foldr f []
+  where f x y | x = x:y
+              | otherwise = y-}
 -- |
 --
 -- >>> map' (+1) [1,2,3]
 -- [2,3,4]
 
 map' :: (a -> b) -> [a] -> [b]
-map' = undefined
+map' f = foldr (\x y -> f x:y) []
 
 -- Optional
 
@@ -82,6 +98,18 @@ myFoldl = undefined
 ----------------------------------------------------------------------
 -- Exercise 4
 ----------------------------------------------------------------------
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
 
-sieveSundaram :: Integer -> [Integer]
-sieveSundaram = undefined
+sieveSundaram :: Int -> [Int]
+sieveSundaram n = map (\x -> 2 * x + 1) $ removeij [1..n] 
+    where
+    possibleij:: Int -> (Int, [(Int, Int)])
+    possibleij x = (x, [(i, j) | 
+                   j <- [1..x `div` 3], 
+                   i <- [1..j], 
+                   i + j + 2 * i * j == x])
+    removeij:: [Int] -> [Int]
+    removeij = map fst 
+                . filter (null . snd)
+                . map possibleij                  
